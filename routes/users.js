02a136a8063ee922a5b07dbb9208715e782e0a31
chuandonghuang <1950439317@qq.com>
2021-12-26 21:34:05 +0800

    数据库及公共接口

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
-
+//router/index.js
+const express = require('express');
+const router = express.Router();
+const user = require('../modules/handle');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'SQL for MySQL' });
+});
+router.get('/addUser', function (req, res, next) {
+  user.add(req, res, next);
+});
+router.get('/queryAll', function (req, res, next) {
+  user.queryAll(req, res, next);
 });
 
+router.get('/query', function (req, res, next) {
+  user.queryById(req, res, next);
+});
+router.get('/deleteUser', function (req, res, next) {
+  user.delete(req, res, next);
+});
+router.get('/update', function (req, res, next) {
+  res.render('update');
+});
+router.post('/updateUser', function (req, res, next) {
+  user.update(req, res, next);
+});
+router.post('/logins', (req, res, next) => {
+  user.logins(req, res, next)
+})
 module.exports = router;
